6ccde7750537f7f05fe6404ffa4c8b5e4420ce50
blackTiles <black4tiles@gmail.com>
2022-10-21 11:40:59 +0530

    prime num added

---
+++ Patterns/PrimeNumber.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool checkPrime(int num){
+    bool isPrime = true;
+    for(int i=2;i<num;i++){
+        if(num%i == 0){
+            isPrime = false;
+            return isPrime;
+        }
+    }
+    return isPrime;
+}
+
+int main(){
+    int number;
+    cout<<"Enter number: ";
+    cin >> number;
+    if(number < 2){
+        cout<<number<<" is not a Prime"<<endl;
+    }
+    else if(checkPrime(number)==true){
+        cout<<number<<" is Prime"<<endl;
+    }
+    else{
+        cout<<number<<" is not a Prime"<<endl;
+    }
+    return 0;
+}
