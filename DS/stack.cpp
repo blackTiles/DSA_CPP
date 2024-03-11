68612da721eb39c72ed0b0ddd5dbc087eeffd7c1
blackTiles <black4tiles@gmail.com>
2024-03-12 01:33:28 +0530

    stack added

--- DS/stack.cpp
+++ DS/stack.cpp
@@ -1,37 +1,62 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int stack[100], n=100, top=-1;
-void push(int val) {
-   if(top>=n-1)
-   cout<<"Stack Overflow"<<endl;
-   else {
-      top++; // top = 0
-      stack[top]=val; // stack[0] = 56
-   }
-}
-void pop() {
-   if(top<=-1)
-   cout<<"Stack Underflow"<<endl;
-   else {
-      cout<<"The popped element is "<< stack[top] <<endl;
-      top--;
-   }
-}
-void display() {
-   if(top>=0) {
-      cout<<"Stack elements are:";
-      for(int i=0; i<=top; i++)
-      cout<<stack[i]<<", ";
-      cout<<endl;
-   } else
-   cout<<"Stack is empty"<<endl;
-}
-int main() {
-    push(14);
-    push(12);
-    push(56);
-    display();
-    pop();
-    display();
+
+template<class T>
+class Stack{
+   private:
+      T *arr = nullptr;
+      int size;
+      int top=-1;
+   public:
+      Stack(int s):size(s){
+         arr = new T[s];
+      }
+      bool isFull(){
+         return top==(size-1);
+      }
+      bool isEmpty(){
+         return top==-1;
+      }
+      void peek(){
+         cout<<"Peek value is : "<<arr[top]<<endl;
+      }
+      void push(T value){
+         if(isFull()){
+            cout<<"Stack Overflow"<<endl;
+         }else{
+            top++;
+            arr[top]=value;
+         }
+      }
+      void pop(){
+         if(isEmpty()){
+            cout<<"Stack Underflow"<<endl;
+         }else{
+            top--;
+         }
+      }
+      void display(){
+         if(isEmpty()){
+            cout<<"Stack Underflow"<<endl;
+         }else{
+            cout<<endl;
+            for(int i=top; i>=0; i--){
+               cout<<"| "<<arr[i]<<" |"<<endl<<"------"<<endl;
+            }
+         }
+      }
+};
+
+int main(){
+   Stack<int> st(5);
+   st.push(10);
+   st.push(11);
+   st.push(12);
+   st.push(13);
+   st.push(14);
+   st.display();
+   st.pop();
+   st.display();
+   st.peek();
    return 0;
 }
